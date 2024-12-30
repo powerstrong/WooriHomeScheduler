0d91013b61969f688783a6fcb328fcdd826a9cba
jslee <jslee@plaif.com>
2024-12-30 22:23:04 +0900

    add mainviewmodel

---
+++ WooriHomeScheduler/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WooriHomeScheduler
+{
+    partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string workers = "1";
+
+    }
+}
