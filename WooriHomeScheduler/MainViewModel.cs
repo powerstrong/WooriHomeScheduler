cb9b8bee40f73593b171471e73f58d40cbd83703
jslee <jslee@plaif.com>
2024-12-30 22:48:33 +0900

    implement input section

--- WooriHomeScheduler/MainViewModel.cs
+++ WooriHomeScheduler/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,15 @@ namespace WooriHomeScheduler
     partial class MainViewModel : ObservableObject
     {
         [ObservableProperty]
-        private string workers = "1";
+        private string workers = string.Empty;
+
+        [ObservableProperty]
+        private string startDate = "2025-01-01";
+
+        [ObservableProperty]
+        private string endDate = "2025-01-01";
+
+        public required ObservableCollection<DateTime> SelectedDates { get; set; }
 
     }
 }
